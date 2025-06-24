a907ca7bdd51f8728f487c45a42debdfb6588610
WinerTy <nikitokmay@gmail.com>
2025-06-24 21:53:15 +0500

    Upd: User routes

--- realtimemap/api/v1/auth/view.py
+++ realtimemap/api/v1/auth/view.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from dependencies.auth.backend import authentication_backend
-from models.user.schemas import UserRead, UserCreate
+from models.user.schemas import UserRead, UserCreate, UserUpdate
 from .fastapi_users import fastapi_users
 
 router = APIRouter(
@@ -15,9 +15,18 @@ router.include_router(
         authentication_backend,
     )
 )
+
 router.include_router(
     router=fastapi_users.get_register_router(
         UserRead,
         UserCreate,
     ),
+)
+
+router.include_router(
+    router=fastapi_users.get_users_router(
+        UserRead, UserUpdate,
+    ),
+    prefix = "/users",
+    tags = ["Users"],
 )
