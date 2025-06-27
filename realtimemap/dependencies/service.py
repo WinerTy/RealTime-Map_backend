6d72e626b8be0d76f19fb6032dfed07999c2ac87
WinerTy <nikitokmay@gmail.com>
2025-06-27 13:39:52 +0500

    Fix: imports

--- realtimemap/dependencies/service.py
+++ realtimemap/dependencies/service.py
@@ -1,17 +1,22 @@
-from typing import Annotated
+from typing import Annotated, TYPE_CHECKING
 
 from fastapi import Depends
 
-from crud.category import CategoryRepository
-from crud.mark import MarkRepository
-from crud.mark_comment.repository import MarkCommentRepository
 from dependencies.crud import (
     get_mark_repository,
     get_category_repository,
     get_mark_comment_repository,
 )
 from dependencies.session import get_session
+from dependencies.websocket import get_mark_websocket_manager
 from services.mark.service import MarkService
+
+if TYPE_CHECKING:
+    from crud.category import CategoryRepository
+    from crud.mark import MarkRepository
+
+    from websocket.mark_socket import MarkManager
+    from crud.mark_comment.repository import MarkCommentRepository
 
 
 async def get_mark_service(
@@ -20,6 +25,7 @@ async def get_mark_service(
     mark_comment_repo: Annotated[
         "MarkCommentRepository", Depends(get_mark_comment_repository)
     ],
+    manager: Annotated["MarkManager", Depends(get_mark_websocket_manager)],
     session: get_session,
 ):
     yield MarkService(
@@ -27,4 +33,5 @@ async def get_mark_service(
         mark_repo=mark_repo,
         category_repo=category_repo,
         mark_comment_repo=mark_comment_repo,
+        manager=manager,
     )
