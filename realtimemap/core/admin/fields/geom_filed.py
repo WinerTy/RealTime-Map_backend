e607592fcb72c9e319106d8112f3dcb32d9a88cf
WinerTy <nikitokmay@gmail.com>
2025-07-18 10:28:42 +0300

    Upd: Field in admin for geom

--- realtimemap/core/admin/fields/geom_filed.py
+++ realtimemap/core/admin/fields/geom_filed.py
@@ -3,21 +3,18 @@ from typing import Any
 from fastapi import Request
 from starlette.datastructures import FormData
 from starlette_admin import RequestAction
-from starlette_admin.fields import BaseField
+from starlette_admin.fields import StringField
 
 from utils.geom_serializator import serialization_geom
 
 
-class GeomField(BaseField):
-    render_input_template = "starlette_admin/fields/geom_input.html"
-    render_view_template = "starlette_admin/fields/geom_view.html"
-    input_type = "text"
+class GeomField(StringField):
 
     def __init__(self, *args, srid=4326, **kwargs):
         super().__init__(*args, **kwargs)
         self.srid = srid
 
-    async def parse_obj(self, request: Request, obj: Any) -> Any:
+    async def parse_obj(self, request: Request, obj: Any) -> str:
         """
         Parses the WKTElement from the database object to be used in the template.
         """
@@ -25,7 +22,27 @@ class GeomField(BaseField):
         if value is None:
             return "Coords not found"
         result = serialization_geom(value)
-        return result.coordinates
+        coords = result.coordinates._asdict()
+
+        return f"{coords.get("latitude")}, {coords.get("longitude")}"
+
+    @staticmethod
+    def _validate_coords(data: str) -> str:
+        try:
+            lat, lon = data.split(",")
+
+            lat = float(lat)
+            lon = float(lon)
+
+            if lat < -90 or lat > 90:
+                raise ValueError
+
+            if lon < -180 or lon > 180:
+                raise ValueError
+            return f"SRID=4326;POINT({lon} {lat})"
+
+        except Exception:
+            raise
 
     async def parse_form_data(
         self, request: Request, form_data: FormData, action: RequestAction
@@ -33,5 +50,9 @@ class GeomField(BaseField):
         """
         Extracts the value of this field from submitted form data.
         """
-        print(form_data.values())
-        return form_data.get(self.id)
+        try:
+            geom = form_data.get(self.id)
+            wkb_coords = self._validate_coords(geom)
+            return wkb_coords
+        except Exception:
+            return None
--- realtimemap/core/admin/model/mark.py
+++ realtimemap/core/admin/model/mark.py
@@ -1,14 +1,11 @@
 from datetime import datetime
 from typing import Dict, Any
 
-from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette_admin import (
-    FloatField,
     HasOne,
     IntegerField,
     DateTimeField,
-    FileField,
 )
 from starlette_admin.contrib.sqla import ModelView
 from starlette_admin.exceptions import FormValidationError
@@ -20,18 +17,7 @@ from models import Mark
 class AdminMark(ModelView):
     fields = [
         Mark.id,
-        GeomField(
-            "geom",
-            srid=4326,
-            exclude_from_create=True,
-            exclude_from_edit=True,
-        ),
-        FloatField(
-            "longitude", exclude_from_list=True, exclude_from_detail=True, required=True
-        ),
-        FloatField(
-            "latitude", exclude_from_list=True, exclude_from_detail=True, required=True
-        ),
+        GeomField("geom", srid=4326, required=True),
         Mark.mark_name,
         HasOne("owner", identity="user", required=True),
         Mark.additional_info,
@@ -51,82 +37,23 @@ class AdminMark(ModelView):
     ) -> None:
         pass
 
-    @staticmethod
-    def convert_geom(data: Dict[str, Any]) -> Dict[str, Any]:
-        data["geom"] = f"SRID=4326;POINT({data['longitude']} {data['latitude']})"
-        del data["longitude"]
-        del data["latitude"]
-        return data
-
     async def validate(self, request: Request, data: Dict[str, Any]) -> None:
         errors: Dict[str, str] = dict()
 
-        if data["longitude"] < -180 or data["latitude"] > 180:
-            errors["longitude"] = "Not valid longitude"
+        if data["geom"] is None:
+            errors["geom"] = "Geom required. Example: 180,90"
 
-        if data["latitude"] < -90 or data["longitude"] > 180:
-            errors["latitude"] = "Not valid latitude"
+        if request.state.action != "EDIT":
+            if data["start_at"] < datetime.now():
+                errors["start_at"] = "Start at is before current time"
 
-        if data["start_at"] < datetime.now():
-            errors["start_at"] = "Not valid start at"
+        if data["owner"] is None:
+            errors["owner"] = "Not valid owner"
+
+        if data["category"] is None:
+            errors["category"] = "Not valid category"
 
         if len(errors) > 0:
             raise FormValidationError(errors)
 
-        valid_data = self.convert_geom(data)
-        return await super().validate(request, valid_data)
-
-    async def create(self, request: Request, data: Dict[str, Any]) -> Any:
-        try:
-            new_data = await self._arrange_data(request, data)
-            await self.validate(request, new_data)
-            session: AsyncSession = request.state.session
-            mark = await self._populate_obj(request, self.model(), new_data)
-            mark.geom = new_data["geom"]
-            session.add(mark)
-            await self.before_create(request, new_data, mark)
-            await session.commit()
-            await session.refresh(mark)
-            await self.after_create(request, mark)
-        except Exception as e:
-            return self.handle_exception(e)
-
-    async def _populate_obj(
-        self,
-        request: Request,
-        obj: Any,
-        data: Dict[str, Any],
-        is_edit: bool = False,
-    ) -> Any:
-        for field in self.get_fields_list(request, request.state.action):
-            if field.name in ["longitude", "latitude"]:
-                continue
-            name, value = field.name, data.get(field.name, None)
-            if isinstance(field, FileField):
-                value, should_be_deleted = value
-                if should_be_deleted:
-                    setattr(obj, name, None)
-                elif (not field.multiple and value is not None) or (
-                    field.multiple and isinstance(value, list) and len(value) > 0
-                ):
-                    setattr(obj, name, value)
-            else:
-                setattr(obj, name, value)
-        return obj
-
-    # async def edit(self, request: Request, pk: Any, data: Dict[str, Any]) -> Any:
-    #     try:
-    #         data = await self._arrange_data(request, data, True)
-    #         await self.validate(request, data)
-    #         session: AsyncSession = request.state.session
-    #         obj = await self.find_by_pk(request, pk)
-    #         await self._populate_obj(request, obj, data, True)
-    #         session.add(obj)
-    #         await self.before_edit(request, data, obj)
-    #         await session.commit()
-    #         await session.refresh(obj)
-    #
-    #         await self.after_edit(request, obj)
-    #         return obj
-    #     except Exception as e:
-    #         self.handle_exception(e)
+        return await super().validate(request, data)
