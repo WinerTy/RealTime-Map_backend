a6f73c02ab2a7e7a8186877bed85c02aa5329f3e
WinerTy <nikitokmay@gmail.com>
2025-08-21 14:52:47 +0300

    Fix: Admin Panel mark create

---
+++ realtimemap/core/admin/fields/geohash_field.py
@@ -0,0 +1,18 @@
+from starlette.datastructures import FormData
+from starlette.requests import Request
+from starlette_admin import StringField, RequestAction
+
+from utils.geom.geom_sector import get_geohash
+
+
+class GeoHashField(StringField):
+
+    async def parse_form_data(
+        self, request: Request, form_data: FormData, action: RequestAction
+    ) -> str:
+        try:
+            lat, lon = form_data.get("geom").split(",")
+            geohash = get_geohash(float(lat), float(lon))
+            return geohash
+        except Exception:
+            return ""
