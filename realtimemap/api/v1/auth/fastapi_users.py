cca6ffa2f71ce7881466a1d2a6acae26214f2f50
WinerTy <nikitokmay@gmail.com>
2025-09-04 08:44:55 +0300

    preparate for add new dependency for user

--- realtimemap/api/v1/auth/fastapi_users.py
+++ realtimemap/api/v1/auth/fastapi_users.py
@@ -16,4 +16,9 @@ fastapi_users = FastAPIUsers[User, int](
 current_active_user = fastapi_users.current_user(active=True)
 current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
 
-current_user = Annotated[User, Depends(current_active_user)]
+
+async def get_current_user(user: User = Depends(current_active_user)):
+    return user
+
+
+current_user = Annotated[User, Depends(get_current_user)]
