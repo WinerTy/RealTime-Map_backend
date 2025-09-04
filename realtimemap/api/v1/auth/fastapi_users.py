ca3090af6611bcb33ba1ff8bcac60fbf06c4d89d
WinerTy <nikitokmay@gmail.com>
2025-09-04 09:55:49 +0300

    Feat: Check Active User ban dependency for endpoints

--- realtimemap/api/v1/auth/fastapi_users.py
+++ realtimemap/api/v1/auth/fastapi_users.py
@@ -1,10 +1,13 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, status
 from fastapi_users import FastAPIUsers
 
+from crud.user.repository import UserRepository
 from dependencies.auth.backend import authentication_backend
 from dependencies.auth.manager import get_user_manager
+from dependencies.crud import get_user_repository
+from exceptions import UserPermissionError
 from models import User
 
 fastapi_users = FastAPIUsers[User, int](
@@ -17,7 +20,16 @@ current_active_user = fastapi_users.current_user(active=True)
 current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
 
 
-async def get_current_user(user: User = Depends(current_active_user)):
+async def get_current_user(
+    user_repo: Annotated[UserRepository, Depends(get_user_repository)],
+    user: User = Depends(current_active_user),
+):
+    user_ban = await user_repo.user_is_banned(user.id)
+    print(user_ban)
+    if user_ban:
+        raise UserPermissionError(
+            detail="You are banned at this time.", status_code=status.HTTP_403_FORBIDDEN
+        )
     return user
 
 
--- realtimemap/crud/user/repository.py
+++ realtimemap/crud/user/repository.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
 
+from sqlalchemy import select, and_, or_
 from starlette.responses import Response
 
 from crud import BaseRepository
-from models import User
+from models import User, UsersBan
 from models.user.schemas import UserCreate, UserRead, UserUpdate
 
 if TYPE_CHECKING:
@@ -19,3 +21,35 @@ class UserRepository(BaseRepository[User, UserCreate, UserRead, UserUpdate]):
 
     async def delete_user(self, user: User) -> Response:
         return await self.delete(user.id)
+
+    async def user_is_banned(self, user_id: int) -> bool:
+        """
+        Check if the user is banned.
+
+        Args:
+            user_id (int): The id of the user.
+
+        Returns:
+            bool: Whether the user is banned.
+        """
+        current_time = datetime.now()
+        stmt = (
+            select(UsersBan)
+            .where(
+                and_(
+                    UsersBan.user_id == user_id,
+                    or_(
+                        UsersBan.is_permanent == True,
+                        and_(
+                            UsersBan.is_permanent == False,
+                            UsersBan.banned_until > current_time,
+                        ),
+                    ),
+                )
+            )
+            .order_by(UsersBan.banned_at.desc())
+        )
+        result = await self.session.execute(stmt)
+        active_ban = result.scalar_one_or_none()
+
+        return active_ban is not None
