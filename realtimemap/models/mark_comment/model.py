f2b475cec6834bd819711bfa8dccca1bf3b48810
WinerTy <nikitokmay@gmail.com>
2025-09-04 08:43:27 +0300

    Feat: Comment table migration

--- realtimemap/models/mark_comment/model.py
+++ realtimemap/models/mark_comment/model.py
@@ -1,6 +1,16 @@
-from typing import TYPE_CHECKING
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
 
-from sqlalchemy import ForeignKey, String, Integer
+from sqlalchemy import (
+    ForeignKey,
+    String,
+    Integer,
+    Boolean,
+    UniqueConstraint,
+    DateTime,
+    func,
+    Index,
+)
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from models import BaseSqlModel
@@ -30,4 +40,65 @@ class MarkComment(BaseSqlModel, IntIdMixin, TimeMarkMixin):
     mark: Mapped["Mark"] = relationship(
         "Mark",
         foreign_keys=[mark_id],
+    )
+
+
+class Comment(BaseSqlModel, IntIdMixin, TimeMarkMixin):
+    content: Mapped[str] = mapped_column(String(256), nullable=False)
+
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # FK
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    mark_id: Mapped[int] = mapped_column(
+        ForeignKey("marks.id", ondelete="CASCADE"), nullable=False
+    )
+    parent_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("comments.id", ondelete="CASCADE"), nullable=True
+    )
+
+    # RS
+    #
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        print("Вызов init")
+
+
+class CommentReaction(BaseSqlModel, IntIdMixin, TimeMarkMixin):
+    # FK
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    comment_id: Mapped[int] = mapped_column(
+        ForeignKey("comments.id", ondelete="CASCADE"), nullable=False
+    )
+    # cols
+    is_like: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # RS
+    #
+    __table_args__ = (
+        UniqueConstraint("user_id", "comment_id", name="uq_user_comment_reaction"),
+    )
+
+
+class CommentStat(BaseSqlModel, IntIdMixin):
+    # FK
+    comment_id: Mapped[int] = mapped_column(
+        ForeignKey("comments.id", ondelete="CASCADE"), nullable=False
+    )
+
+    # cols
+    likes_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    dislikes_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    total_replies: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    last_activity: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+    # RS
+    #
+    __table_args__ = (
+        Index("ix_comment_stats_likes", "likes_count"),
+        Index("ix_comment_stats_activity", "last_activity"),
     )
--- realtimemap/models/mark_comment/schemas.py
+++ realtimemap/models/mark_comment/schemas.py
@@ -1,36 +1,30 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 from models.user.schemas import UserRead
 
 
-class BaseMarkComment(BaseModel):
+class CreateComment(BaseModel):
+    content: str
+    owner_id: int
+    mark_id: int
+    parent_id: Optional[int] = None
+
+
+class UpdateComment(BaseModel):
+    content: str
+    owner_id: int
+    mark_id: int
+
+
+class DeleteComment(BaseModel):
+    content: str
+    owner_id: int
+    mark_id: int
+
+
+class CreateCommentRequest(BaseModel):
     content: str = Field(..., description="Mark content", min_length=1, max_length=256)
-
-
-class CreateMarkComment(BaseMarkComment):
-    mark_id: int = Field(..., description="Mark id", ge=0)
-    user_id: int = Field(..., description="User id", ge=0)
-
-
-class UpdateMarkCommentReaction(BaseModel):
-    likes: int = Field(..., description="Likes", ge=0)
-    dislikes: int = Field(..., description="Dislikes", ge=0)
-
-
-class UpdateMarkComment(BaseMarkComment, UpdateMarkCommentReaction):
-    pass
-
-
-class ReadMarkComment(BaseMarkComment):
-    mark_id: int = Field(..., description="Mark id", ge=0)
-    likes: int = Field(..., description="Likes", ge=0)
-    dislikes: int = Field(..., description="Dislikes", ge=0)
-    created_at: datetime = Field(..., description="Creation date in ISO format")
-    user: Optional[UserRead] = None
-
-
-class CreateMarkCommentRequest(BaseMarkComment):
-    pass
+    parent_id: Optional[int] = Field(None, description="Parent Mark id", ge=0)
