7320927cce5ead6eee24ffef60e3903a6773294c
WinerTy <nikitokmay@gmail.com>
2025-09-05 10:52:19 +0300

    Feat: Auth in admin

--- realtimemap/core/admin/admin.py
+++ realtimemap/core/admin/admin.py
@@ -1,18 +1,31 @@
 from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
 from starlette_admin.contrib.sqla import Admin
 
+from core.admin.auth.provider import AdminAuthProvider
 from core.admin.model import (
     AdminCategory,
     AdminUser,
     AdminMark,
 )
+from core.config import conf
 from database.helper import db_helper
 from models import Category, User, Mark
 
 
 def setup_admin(app: FastAPI) -> None:
     # Fix статики в адм на хостинге
-    admin = Admin(engine=db_helper.engine, title="RealTime-Map")
+    admin = Admin(
+        engine=db_helper.engine,
+        title="RealTime-Map",
+        auth_provider=AdminAuthProvider(),
+        middlewares=[
+            Middleware(
+                SessionMiddleware, secret_key=conf.api.v1.auth.verification_token_secret
+            )
+        ],
+    )
     admin.add_view(AdminCategory(Category))
     admin.add_view(AdminUser(User))
     admin.add_view(AdminMark(Mark))
--- realtimemap/main.py
+++ realtimemap/main.py
@@ -9,6 +9,7 @@ from starlette.responses import FileResponse, StreamingResponse
 
 from core.app import create_app
 from core.config import conf
+from exceptions import UserPermissionError
 
 app = create_app()
 
