bfad0a47a7eed98f14437905643c93c313a391d1
WinerTy <nikitokmay@gmail.com>
2025-09-13 15:23:44 +0300

    Migrate + models

--- realtimemap/api/v1/__init__.py
+++ realtimemap/api/v1/__init__.py
@@ -7,6 +7,7 @@ from .auth import router as auth_router
 from .category.view import router as category_router
 from .docs.view import router as docs_router
 from .mark import router as mark_router
+from .users.chats import router as users_chat_router
 from .users.view import router as users_router
 
 router = APIRouter(prefix=conf.api.v1.prefix)
@@ -14,4 +15,5 @@ router.include_router(auth_router)
 router.include_router(mark_router)
 router.include_router(category_router)
 router.include_router(users_router)
+router.include_router(users_chat_router)
 router.include_router(docs_router)
