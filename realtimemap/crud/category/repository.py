c50beeb82616621d5b22cf6630da0989a4ff41ac
WinerTy <nikitokmay@gmail.com>
2025-10-17 19:36:16 +0300

    Upd: Errors

--- realtimemap/crud/category/repository.py
+++ realtimemap/crud/category/repository.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Optional, Sequence
 
 from sqlalchemy import select
 
@@ -14,7 +14,7 @@ class CategoryRepository(BaseRepository[Category, CreateCategory, UpdateCategory
     def __init__(self, session: "AsyncSession"):
         super().__init__(session=session, model=Category)
 
-    async def get_all(self) -> List[Category]:
+    async def get_all(self) -> Sequence[Category]:
         stmt = select(self.model)
         result = await self.session.execute(stmt)
         result = result.scalars().all()
