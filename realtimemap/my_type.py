3c5262e852d70ec8f2d748f8ade6722e87b5621f
WinerTy <nikitokmay@gmail.com>
2025-11-20 00:20:32 +0500

    compleat todo

--- realtimemap/my_type.py
+++ realtimemap/my_type.py
@@ -1,15 +1,9 @@
-from typing import TypeVar, Protocol
+from typing import TypeVar
 
 from pydantic import BaseModel
 
 from modules import BaseSqlModel
 
-
-class ModelWithId(Protocol):
-    id: int
-
-
-# TODO CHANGE TO BASESQL CLASS
 Model = TypeVar("Model", bound=BaseSqlModel)
 CreateSchema = TypeVar("CreateSchema", bound=BaseModel)
 ReadSchema = TypeVar("ReadSchema", bound=BaseModel)
